2343a4c6e33b4e61cc470f5eb688473e3e706b34
Jonathan Mesa <53017957+jonathanmesa@users.noreply.github.com>
2020-05-12 08:53:46 -0500

    Update SelectString.ps1

--- day2/SelectString.ps1
+++ day2/SelectString.ps1
@@ -2,10 +2,10 @@
 netsh dump | Select-String -Pattern "^set"
 
 #Show all interfaces using netsh
-netsh trace show interfaces | Select-String -Pattern "^\s+Description“
+netsh trace show interfaces | Select-String -Pattern "^\s+Description"
 
 #Show all interfaces using netsh and use alternation to add the index
-netsh trace show interfaces | Select-String -Pattern "^\s+Description|index“
+netsh trace show interfaces | Select-String -Pattern "^\s+Description|index"
 
 #Mixing it all together: replace the description using capture groups
 (netsh trace show interfaces | Select-String -Pattern "^\s+Description|index") 
