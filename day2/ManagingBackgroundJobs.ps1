9ab29f3016ca328c2f53f63c3ad1a3f9fbf67604
Jonathan Mesa <53017957+jonathanmesa@users.noreply.github.com>
2020-05-12 08:52:46 -0500

    Update ManagingBackgroundJobs.ps1

--- day2/ManagingBackgroundJobs.ps1
+++ day2/ManagingBackgroundJobs.ps1
@@ -1,4 +1,4 @@
-﻿#Start a background job and wait for the job to finish. ;
+#Start a background job and wait for the job to finish. ;
 Start-Job –Name MyWaitingJob –ScriptBlock {Get-WinEvent –LogName Application –MaxEvents 1000} | wait-job
 
 #Receive the results:
@@ -8,10 +8,11 @@ Get-job –Name MyWaitingJob | Receive-Job
 Get-job –Name MyWaitingJob | Receive-Job
 
 #Start a background job but don’t wait it to finish:
-Start-Job –Name MyWaitingJob –ScriptBlock {Get-WinEvent –LogName Application –MaxEvents 5000
+Start-Job –Name MyWaitingJob –ScriptBlock {Get-WinEvent –LogName Application –MaxEvents 5000 }
 
 #Run the following line multiple times over and over:
 (Get-job –Name MyWaitingJob | Receive-Job –Keep).Count
 
 ##########
+
 
